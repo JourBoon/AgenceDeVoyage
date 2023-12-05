74014978443c216fff533c6a8d6f072f257f0564
JourBoon <unconfigured@null.spigotmc.org>
2023-12-05 10:27:56 +0100

    static css

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__, static_folder='public/static', template_folder='templates')
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
     yo = "Home Page"
     return render_template("home.html", voyages=yo)
 
-@app.route('/reservation')
+@app.route('/reserver.html')
 def reserver():
     yo = "Reserver"
     return render_template("reserver.html", voyages=yo)
