2858c8d6ef131a418ddb6c992a0e0e10966a2ded
JourBoon <unconfigured@null.spigotmc.org>
2023-12-08 09:29:09 +0100

    created db tables

--- database/init_datasets.sql
+++ database/init_datasets.sql
@@ -1,9 +1,52 @@
-CREATE TABLE ma_table (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE CLIENT (
+    id_client INTEGER PRIMARY KEY AUTOINCREMENT,
+    prenom TEXT,
     nom TEXT,
-    age INTEGER
+    age INTEGER,
+    adresse TEXT,
+    mail TEXT,
+    tel_num INT
 );
 
+CREATE TABLE DESTINATION (
+    id_dest INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom_dest TEXT,
+    desc_dest TEXT,
+    cost INT,
+    tel_num INT,
+    places INT,
+    duree INT,
+);
 
-INSERT INTO ma_table (nom, age) VALUES ('John Doe', 25);
-INSERT INTO ma_table (nom, age) VALUES ('Jane Doe', 30);
+CREATE TABLE ACTIVITE (
+    id_act INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_dest INTEGER FOREIGN KEY id_dest REFERENCES DESTINATION(id_dest),
+    nom_act TEXT,
+    sup_cost INT,
+    duree_act INT,
+    date_act DATE,
+    desc_act TEXT
+);
+
+CREATE TABLE VOYAGE (
+    id_voy INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_dest INTEGER FOREIGN KEY id_dest REFERENCES DESTINATION(id_dest),
+    date_depart DATE,
+    date_retour DATE,
+    places INT,
+    cost INT
+);
+
+CREATE TABLE RESERVATION (
+    id_res INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_cient INTEGER FOREIGN KEY id_client REFERENCES CLIENT(id_client),
+    id_dest INTEGER FOREIGN KEY id_dest REFERENCES DESTINATION(id_dest),
+    cost INT
+);
+
+CREATE TABLE PAIEMENT (
+    id_paim INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_res INTEGER FOREIGN KEY id_res REFERENCES RESERVATION(id_res),
+    cost INT,
+    paye_date DATE
+);
