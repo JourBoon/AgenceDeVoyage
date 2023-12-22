b6c84081deb11acd98d765c496436bffb8b4f9a8
Fundhes <salastuce@gmail.com>
2023-12-22 09:19:55 +0100

    auth

--- auth.py
+++ auth.py
@@ -1,19 +1,24 @@
+import sys
+sys.path.append('static/services')
+sys.path.append('static/models')
+sys.path.append('static/services/exceptions')
 from app import app
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Flask, Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import User
-from . import db
-...
+from database import db
 
-@app.route('/connexion')
+auth = Blueprint('auth', __name__)
+
+@auth.route('/connexion')
 def login():
     return render_template('connexion.html')
 
-@app.route('/inscription')
+@auth.route('/inscription')
 def signup():
     return render_template('inscription.html')
 
-@app.route('/inscription', methods=['POST'])
+@auth.route('/inscription', methods=['POST'])
 def signup_post():
     # code to validate and add user to database goes here
     email = request.form.get('email')
@@ -24,7 +29,7 @@ def signup_post():
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')
-        return redirect(url_for('app.inscription'))
+        return redirect(url_for('auth.inscription'))
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
@@ -33,4 +38,4 @@ def signup_post():
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect(url_for('app.connexion'))
+    return redirect(url_for('auth.connexion'))
